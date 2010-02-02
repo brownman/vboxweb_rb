804b6980b585e55812f85dbac665dc15ae2007fa
Kieran Pilkington <kieran@katipo.co.nz>
2010-02-02 18:19:52 +1300

    Whoops, wrong log path for .gitignore. Commited some logs in the prevous commit. Removing now.

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,10 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+# Development only
+# $:<<File.join(Rails.root, 'virtualbox', 'lib')
+# require 'virtualbox'
+
 Rails::Initializer.run do |config|
   config.gem 'less'
   config.gem 'virtualbox'
@@ -14,7 +18,3 @@ Rails::Initializer.run do |config|
 
   config.time_zone = 'UTC'
 end
-
-# Development only
-# $:<<File.join(Rails.root, 'virtualbox', 'lib')
-# require 'virtualbox'
