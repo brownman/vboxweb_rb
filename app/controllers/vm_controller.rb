842554ab7f248862cd720f044018d4c7b66e6ad6
Kieran Pilkington <kieran@katipo.co.nz>
2010-02-12 18:24:16 +1300

    Don't raise errors because there is a current double request bug. The command works first time, then throws an error.

--- app/controllers/vm_controller.rb
+++ app/controllers/vm_controller.rb
@@ -8,25 +8,25 @@ class VmController < ApplicationController
 
     case params[:command]
     when 'start'
-      @vm.start(:headless, true)
+      @vm.start(:headless)
       flash[:notice] = "#{@vm.name} has been started, but give it a minute to fully boot."
     when 'shutdown'
-      @vm.shutdown(true)
+      @vm.shutdown
       flash[:notice] = "#{@vm.name} is being shutdown. Please wait a bit for it to complete and refresh the page."
     when 'poweroff'
-      @vm.stop(true)
+      @vm.stop
       flash[:notice] = "#{@vm.name} has been powered off. Any unsaved data will have been lost."
     when 'pause'
-      @vm.pause(true)
+      @vm.pause
       flash[:notice] = "#{@vm.name} has been paused."
     when 'resume'
-      @vm.resume(true)
+      @vm.resume
       flash[:notice] = "#{@vm.name} has been resumed."
     when 'save'
-      @vm.save_state(true)
+      @vm.save_state
       flash[:notice] = "#{@vm.name} has been saved. You may resume at any time by pressing 'Start'."
     when 'discard'
-      @vm.discard_state(true)
+      @vm.discard_state
       flash[:notice] = "#{@vm.name} saved state has been discarded."
     else
       flash[:error] = "Unsupported Virtual Machine Operation '#{params[:action]}'"
