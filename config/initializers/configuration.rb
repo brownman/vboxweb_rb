4264d6089e57d7328e8faf70e86b0df33872daa1
Kieran Pilkington <kieran@katipo.co.nz>
2010-02-16 20:16:37 +1300

    Adding vboxconfig to config.yml, adding documentation

--- config/initializers/configuration.rb
+++ config/initializers/configuration.rb
@@ -1,2 +1,3 @@
 APP_CONFIG = YAML.load(IO.read(File.join(Rails.root, 'config', 'config.yml')))
-VirtualBox::Command.vboxmanage = APP_CONFIG['vboxmanage']
+VirtualBox::Command.vboxmanage = APP_CONFIG['vboxmanage'] unless APP_CONFIG['vboxmanage'].blank?
+VirtualBox::Global.vboxconfig = APP_CONFIG['vboxconfig'] unless APP_CONFIG['vboxconfig'].blank?
